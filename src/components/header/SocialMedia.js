3d6c3a556e18a9d88ffb5686cfce5902cf851a9c
Alison Rodrigues <59927590+nosilaepilef@users.noreply.github.com>
2025-04-02 12:22:33 -0400

    Update SocialMedia.js

--- src/components/header/SocialMedia.js
+++ src/components/header/SocialMedia.js
@@ -1,4 +1,4 @@
-import {FaInstagram, FaTiktok} from 'react-icons/fa'
+import {FaInstagram, FaTiktok, FaGooglePlay, FaApple} from 'react-icons/fa'
 
 function SocialMedia() {
 
@@ -8,6 +8,8 @@ function SocialMedia() {
                 <div className="social-midia">
                     <a target='_blank' href='https://www.instagram.com/ecellacessorios'><FaInstagram size={20} color='#f0f0f0'/></a>
                     <a target='_blank' href='https://www.instagram.com/ecellshopbarber'><FaInstagram size={20} color='#f0f0f0'/></a>
+                    <a target='_blank' href='https://play.google.com/store/apps/details?id=br.inf.ecell.ecellacess&pcampaignid=web_share'><FaGooglePlay size={20} color='#f0f0f0'/></a>
+                    <a target='_blank' href='https://apps.apple.com/app/id1531213629'><FaApple size={20} color='#f0f0f0'/></a>
                 </div>
             </div>
         </nav>
